005d075747d57b13e9c4e01d60d7f9a10e45ebff
Kurtis Leach <danceflurry4366@gmail.com>
2019-05-09 02:09:02 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,10 +36,9 @@ end
 end
 
 def for_iterator(number_of_times)
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  times = (1...7)
-  for items in times do
-    puts phrase
-  end
+  counter=1..number_of_times
+   for item in counter do
+     puts phrase
+   end
 
 end
